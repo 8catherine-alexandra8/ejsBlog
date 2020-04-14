b72c39f6747e87bd8b6eed419bfc097816e45276
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-04-14 08:40:11 -0600

    complete challenge 11. create array to contain all posts, push post object into it from compose page, redirect to home page and console log posts array

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const aboutContent =
 const contactContent =
 	'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
+const posts = [];
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -22,6 +23,7 @@ app.get('/', function(req, res) {
 	res.render('home', {
 		homeParagraph : homeStartingContent
 	});
+	console.log(posts);
 });
 
 app.get('/contact', function(req, res) {
@@ -41,12 +43,12 @@ app.get('/compose', function(req, res) {
 });
 
 app.post('/compose', function(req, res) {
-	let post = {
-		title : req.body.postTitle,
-		body  : req.body.postBody
+	const post = {
+		title   : req.body.postTitle,
+		content : req.body.postBody
 	};
-	console.log(post);
-	res.redirect('/compose');
+	posts.push(post);
+	res.redirect('/');
 });
 
 app.listen(3000, function() {
